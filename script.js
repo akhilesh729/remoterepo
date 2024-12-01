4d64fd448d6baa47163fb3fea0e4bf987fa5c184
akhilesh <akhileshpathk920@gmail.com>
2024-12-01 21:53:17 +0530

    my first commit

---
+++ script.js
@@ -0,0 +1,77 @@
+// for(let i=0;i<=100;i++){
+// console.log("hello world");
+// }
+
+// let i=0;
+// while(i<=8){
+//     console.log("akhilesh pathak");
+//     i++; 
+// }
+
+let i=0;
+do{
+    console.log("akhilesh pathak");
+    i++; 
+}while(i<=10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// fullname="Akhilesh Pathak";
+// age=21;
+// a=null;
+// b=undefined;
+// isfollow=true;
+// console.log(fullname);
+// console.log(age);
+// console.log(a);
+// console.log(b);
+// console.log(isfollow);
+// Console="akhilesh pathak";
+// console.log(Console);
+// let name="aman bhai";
+// let age=21;
+// let totalprice=1000;
+// console.log(name);
+
+
+
+
+
+
